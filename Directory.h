8f9de58faf3d9f15d3f4f570525ac3f30f2b4666
quoc-trung-nguyen-hcmus <21120350@student.hcmus.edu.vn>
2023-04-28 16:22:07 +0700

    Updated Message

---
+++ Directory.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ObjectMessage/DataTransferObj.h"
+#include "ObjectMessage/DIR_TransferObj.h"
+
+
+class Directory
+{
+
+
+
+
+
+};
---
+++ ObjectMessage/DIR_TransferObj.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "ObjectMessage/DataTransferObj.h"
+
+class DIR_TransferObj : public DataTransferObj
+{
+
+
+
+
+
+};
