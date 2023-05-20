bd62a5a933e8ade68ee5925c9e4cf3a258eae17a
quoc-trung-nguyen-hcmus <121560414+quoc-trung-nguyen-hcmus@users.noreply.github.com>
2023-05-20 17:01:01 +0700

    Directory Tree

--- source/DIR_Func.h
+++ source/DIR_Func.h
@@ -1,8 +1,67 @@
 #pragma once
 #include <string>
 #include "DataObj/DataObj.h"
-#include "DataObj/KLG_Obj.h"
 #include <vector>
+#include <filesystem>
+#include <iostream>
+#include <string>
 
+
+class DIR_Func
+{
+public:
+    void print_directory_tree(const fs::path& path = fs::current_path(), int max_level = -1, bool include_files = true, int level = 0) {
+        try {
+            if (!fs::exists(path) || !fs::is_directory(path)) return;
+            for (const auto& entry : fs::directory_iterator(path)) {
+                const auto filename = entry.path().filename().string();
+                if (entry.is_directory()) {
+                    std::cout << std::string(level * 2, ' ') << level << " [+] " << filename << '\n';
+                    if (max_level <= 0 || level + 1 < max_level) {
+                        print_directory_tree(entry, max_level, include_files, level + 1);
+                    }
+                }
+                else if (include_files && entry.is_regular_file()) {
+                    std::cout << std::string(level * 2, ' ') << level << " - " << filename << '\n';
+                }
+            }
+        }
+        catch (const std::exception& e) {
+            //std::cerr << e.what() << '\n';
+        }
+    }
+
+    json directory_tree_to_json(const fs::path& path = fs::current_path(), int max_level = -1, bool include_files = true, int level = 0) {
+        json result;
+        try {
+            if (!fs::exists(path) || !fs::is_directory(path)) return result;
+            result["name"] = path.filename().string();
+            result["level"] = level;
+            result["type"] = "directory";
+            for (const auto& entry : fs::directory_iterator(path)) {
+                if (entry.is_directory()) {
+                    if (max_level <= 0 || level + 1 < max_level) {
+                        result["children"].push_back(directory_tree_to_json(entry, max_level, include_files, level + 1));
+                    }
+                }
+                else if (include_files && entry.is_regular_file()) {
+                    json file;
+                    file["name"] = entry.path().filename().string();
+                    file["level"] = level + 1;
+                    file["type"] = "file";
+                    result["children"].push_back(file);
+                }
+            }
+        }
+        catch (const std::exception& e) {
+            //std::cerr << e.what() << '\n';
+        }
+        return result;
+    }
+
+
+
+
+};
 
 
--- source/mains/Trung.cpp
+++ source/mains/Trung.cpp
@@ -9,26 +9,71 @@ Don't change this file  without informing the author
 #include <vector>
 #include "../DataObj/DataObj.h"
 #include "../utils.h"
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <filesystem>
 
+namespace fs = std::filesystem;
 using json = nlohmann::json;
 
 using  namespace std;
 
 
-#include <chrono>
-#include <ctime>
-#include <iomanip>
+
+void print_directory_tree(const fs::path& path = fs::current_path(), int max_level = -1, bool include_files = true, int level = 0) {
+    try {
+        if (!fs::exists(path) || !fs::is_directory(path)) return;
+        for (const auto& entry : fs::directory_iterator(path)) {
+            const auto filename = entry.path().filename().string();
+            if (entry.is_directory()) {
+                std::cout << std::string(level * 2, ' ') << level << " [+] " << filename << '\n';
+                if (max_level <= 0 || level + 1 < max_level) {
+                    print_directory_tree(entry, max_level, include_files, level + 1);
+                }
+            }
+            else if (include_files && entry.is_regular_file()) {
+                std::cout << std::string(level * 2, ' ') << level << " - " << filename << '\n';
+            }
+        }
+    }
+    catch (const std::exception& e) {
+        //std::cerr << e.what() << '\n';
+    }
+}
+
+json directory_tree_to_json(const fs::path& path = fs::current_path(), int max_level = -1, bool include_files = true, int level = 0) {
+    json result;
+    try {
+        if (!fs::exists(path) || !fs::is_directory(path)) return result;
+        result["name"] = path.filename().string();
+        result["level"] = level;
+        result["type"] = "directory";
+        for (const auto& entry : fs::directory_iterator(path)) {
+            if (entry.is_directory()) {
+                if (max_level <= 0 || level + 1 < max_level) {
+                    result["children"].push_back(directory_tree_to_json(entry, max_level, include_files, level + 1));
+                }
+            }
+            else if (include_files && entry.is_regular_file()) {
+                json file;
+                file["name"] = entry.path().filename().string();
+                file["level"] = level + 1;
+                file["type"] = "file";
+                result["children"].push_back(file);
+            }
+        }
+    }
+    catch (const std::exception& e) {
+        //std::cerr << e.what() << '\n';
+    }
+    return result;
+}
 
 int main() {
-	
-
-std:shared_ptr<DataObj> obj(new DataObj(utils::CurrentTime(), REQUEST, KLG, STOP, "Never gonna give you up"));
-	std::cout << obj->toString();
-
-
-
-
-
-	system("pause");
-	return 0;
+    std::cout << directory_tree_to_json("C:\\", 2).dump(4) << '\n';
+    
+    
+    system("pause");
+    return 0;
 }
