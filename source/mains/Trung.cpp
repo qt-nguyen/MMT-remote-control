1f00d1300a9ea8346da85be8c5159cd9004ae03f
quoc-trung-nguyen-hcmus <121560414+quoc-trung-nguyen-hcmus@users.noreply.github.com>
2023-05-20 10:50:47 +0700

    Updated Trung

--- source/mains/Trung.cpp
+++ source/mains/Trung.cpp
@@ -3,7 +3,7 @@ This is Trung's demo to test functionalities and dependencies
 Please exclude this file from project if you want to run your own main demo
 Don't change this file  without informing the author
 */
-
+#include <nlohmann/json.hpp>
 #include <iostream>
 
 using  namespace std;
