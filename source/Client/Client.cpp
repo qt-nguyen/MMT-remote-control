06825afc610ad67476700b787dc08b3c6d74efc8
ThanhKy <61177006+thanh634@users.noreply.github.com>
2023-05-25 08:49:30 +0700

    update client's getClientData() -- add case 0 : exit / go back from cmd

--- source/Client/Client.cpp
+++ source/Client/Client.cpp
@@ -65,151 +65,180 @@ void Client::getClientData()
     FuncType funcType = FUNC_TYPE;
     CmdType cmdType = CMD_TYPE;
     std::string data = "";
-
-    std::cout << "Please select the funtion you want to run : \n"
-        " 1 : IAP  --  Installed Application \n"
-        " 2 : RPC  --  Running Processes \n"
-        " 3 : SCR  --  Screen Capturing \n"
-        " 4 : KLG  --  Keylogger \n"
-        " 5 : DIR  --  Directory Tree \n";
     do
     {
-        std::cout << "\n --> YOUR CHOICE : ";
-        std::cin >> choice;
-        if (choice > 5 || choice < 1) std::cout << "\nWRONG FUNCTION! PLEASE INPUT AGAIN.\n";
-    }
-    while(choice > 5 || choice < 1);
-
-    std::cout << "\n What does this function do ? Please select a command...\n";
-    if (choice == 1) {
-        funcType = IAP;
-        std::cout << " 1 : SHOW  --> Show list of installed apps \n"
-            " 2 : START --> Start an app \n"
-            " 3 : STOP  --> Close an app (if opened) \n";
-
+        std::cout << "Please select the funtion you want to run : \n"
+            " 0 : Exit \n"
+            " 1 : IAP  --  Installed Application \n"
+            " 2 : RPC  --  Running Processes \n"
+            " 3 : SCR  --  Screen Capturing \n"
+            " 4 : KLG  --  Keylogger \n"
+            " 5 : DIR  --  Directory Tree \n";
         do
         {
             std::cout << "\n --> YOUR CHOICE : ";
             std::cin >> choice;
-            if (choice > 3 || choice < 1) std::cout << "\nWRONG COMMAND! PLEASE INPUT AGAIN.\n";
-        }
-        while (choice > 3 || choice < 1);
+            if (choice > 5 || choice < 0) std::cout << "\nWRONG FUNCTION! PLEASE INPUT AGAIN.\n";
+        } while (choice > 5 || choice < 0);
 
-        if (choice == 1)
-        {
-            cmdType = SHOW;
-        }
-        else 
-        {
-            if (choice == 2)
+        std::cout << "\nWhat does this function do ? Please select a command...\n\n 0 : Go Back \n";
+        if (choice == 1) {
+            funcType = IAP;
+            std::cout << " 1 : SHOW  --> Show list of installed apps \n"
+                " 2 : START --> Start an app \n"
+                " 3 : STOP  --> Close an app (if opened) \n";
+
+            do
             {
-                cmdType = START;
+                std::cout << "\n --> YOUR CHOICE : ";
+                std::cin >> choice;
+                if (choice > 3 || choice < 0) std::cout << "\nWRONG COMMAND! PLEASE INPUT AGAIN.\n";
+            } while (choice > 3 || choice < 0);
+            if (choice == 0) {
+                funcType = FUNC_TYPE;
             }
-            else if (choice == 3)
+            else
             {
-                cmdType = STOP;
+                if (choice == 1)
+                {
+                    cmdType = SHOW;
+                }
+                else
+                {
+                    if (choice == 2)
+                    {
+                        cmdType = START;
+                    }
+                    else if (choice == 3)
+                    {
+                        cmdType = STOP;
+                    }
+                    std::cout << " Input the name of the app : ";
+                    std::cin >> data;
+                }
             }
-            std::cout << " Input the name of the app : ";
-            std::cin >> data;
         }
-    }
-    else if (choice == 2)
-    {
-        funcType = RPC;
-        std::cout << " 1 : SHOW  --> Show list of running processes \n"
-            " 2 : RUN --> Run a process \n"
-            " 3 : KILL  --> Stop a process (if run) \n";
+        else if (choice == 2)
+        {
+            funcType = RPC;
+            std::cout << " 1 : SHOW  --> Show list of running processes \n"
+                " 2 : RUN --> Run a process \n"
+                " 3 : KILL  --> Stop a process (if run) \n";
 
-        do
-        {
-            std::cout << "\n --> YOUR CHOICE : ";
-            std::cin >> choice;
-            if (choice > 3 || choice < 1) std::cout << "\nWRONG COMMAND! PLEASE INPUT AGAIN.\n";
-        } while (choice > 3 || choice < 1);
-
-        if (choice == 1)
-        {
-            cmdType = SHOW;
-        }
-        else
-        {
-            if (choice == 2)
+            do
             {
-                cmdType = RUN;
+                std::cout << "\n --> YOUR CHOICE : ";
+                std::cin >> choice;
+                if (choice > 3 || choice < 0) std::cout << "\nWRONG COMMAND! PLEASE INPUT AGAIN.\n";
+            } while (choice > 3 || choice < 0);
+
+            if (choice == 0) {
+                funcType = FUNC_TYPE;
             }
-            else if (choice == 3)
+            else
             {
-                cmdType =KILL;
+                if (choice == 1)
+                {
+                    cmdType = SHOW;
+                }
+                else
+                {
+                    if (choice == 2)
+                    {
+                        cmdType = RUN;
+                    }
+                    else if (choice == 3)
+                    {
+                        cmdType = KILL;
+                    }
+                    std::cout << " Input the name of the process : ";
+                    std::cin >> data;
+                }
             }
-            std::cout << " Input the name of the process : ";
-            std::cin >> data;
         }
-    }
-    else if (choice == 3)
-    {
-        funcType = SCR;
-        std::cout << " 1 : START  --> Start live screen capturing session \n"
-            " 2 : STOP  --> Stop live session \n";
-
-        do
+        else if (choice == 3)
         {
-            std::cout << "\n --> YOUR CHOICE : ";
-            std::cin >> choice;
-            if (choice > 2 || choice < 1) std::cout << "\nWRONG COMMAND! PLEASE INPUT AGAIN.\n";
-        } while (choice > 2 || choice < 1);
+            funcType = SCR;
+            std::cout << " 1 : START  --> Start live screen capturing session \n"
+                " 2 : STOP  --> Stop live session \n";
 
-        if (choice == 1)
-        {
-            cmdType = START;
+            do
+            {
+                std::cout << "\n --> YOUR CHOICE : ";
+                std::cin >> choice;
+                if (choice > 2 || choice < 0) std::cout << "\nWRONG COMMAND! PLEASE INPUT AGAIN.\n";
+            } while (choice > 2 || choice < 0);
+
+            if (choice == 0) {
+                funcType = FUNC_TYPE;
+            }
+            else
+            {
+                if (choice == 1)
+                {
+                    cmdType = START;
+                }
+                else
+                {
+                    cmdType = STOP;
+                }
+            }
         }
-        else
+        else if (choice == 4)
         {
-            cmdType = STOP;
+            funcType = KLG;
+            std::cout << " 1 : START  --> Start capturing keys \n"
+                " 2 : STOP  --> Stop capturing keys \n";
+
+            do
+            {
+                std::cout << "\n --> YOUR CHOICE : ";
+                std::cin >> choice;
+                if (choice > 2 || choice < 1) std::cout << "\nWRONG COMMAND! PLEASE INPUT AGAIN.\n";
+            } while (choice > 2 || choice < 1);
+
+            if (choice == 0) {
+                funcType = FUNC_TYPE;
+            }
+            else
+            {
+                if (choice == 1)
+                {
+                    cmdType = START;
+                }
+                else
+                {
+                    cmdType = STOP;
+                }
+            }
         }
-    }
-    else if (choice == 4)
-    {
-        funcType = KLG;
-        std::cout << " 1 : START  --> Start capturing keys \n"
-            " 2 : STOP  --> Stop capturing keys \n";
-
-        do
+        else if (choice == 5)
         {
-            std::cout << "\n --> YOUR CHOICE : ";
-            std::cin >> choice;
-            if (choice > 2 || choice < 1) std::cout << "\nWRONG COMMAND! PLEASE INPUT AGAIN.\n";
-        } while (choice > 2 || choice < 1);
+            std::string max_depth = "-1";
+            std::string include_file = "0";
+            std::string path = "";
 
-        if (choice == 1)
-        {
-            cmdType = START;
+            funcType = DIR;
+            std::cout << " 1 : SHOW  --> Show directory tree with the following parameters\n"
+                " MAX_DEPTH : the maximum depth of the directory tree, if user's input is negative, default = -1 -> show in whole\n"
+                " INCLUDE_FILE : specifies whether to include files or not ( 1 = TRUE, 0 = FALSE)\n"
+                " PATH : path to the root of the directory, if no input recieved -> default to current directory\n";
+            
+            if (choice == 0) {
+                funcType = FUNC_TYPE;
+            }
+            else
+            {
+                cmdType = SHOW;
+                std::cout << " MAX_DEPTH = "; std::cin >> max_depth;
+                std::cout << "INCLUDE_FILE = "; std::cin >> include_file;
+                std::cout << "PATH = "; std::cin >> path;
+
+                data = max_depth + " " + include_file + " " + path;
+            }
         }
-        else
-        {
-            cmdType = STOP;
-        }
-    }
-    else if (choice == 5)
-    {
-        std::string max_depth = "-1";
-        std::string include_file = "0";
-        std::string path = "";
-
-        funcType = DIR;
-        std::cout << " only SHOW  --> Show directory tree with the following parameters\n"
-            " MAX_DEPTH : the maximum depth of the directory tree, if user's input is negative, default = -1 -> show in whole\n"
-            " INCLUDE_FILE : specifies whether to include files or not ( 1 = TRUE, 0 = FALSE)\n"
-            " PATH : path to the root of the directory, if no input recieved -> default to current directory\n";
-
-        cmdType = SHOW;
-        std::cout << " MAX_DEPTH = "; std::cin >> max_depth;
-        std::cout << "INCLUDE_FILE = "; std::cin >> include_file;
-        std::cout << "PATH = "; std::cin >> path;
-
-        data = max_depth + " " + include_file + " " + path;
-    }
-    else funcType = FUNC_TYPE;
-
+        else funcType = FUNC_TYPE;
+    }while (choice == 0);
     _clientData.setID(ID);
     _clientData.setDataType(dataType);
     _clientData.setFuncType(funcType);
